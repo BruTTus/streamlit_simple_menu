bacb6a8b7298472cdd4bae05e751bd3146e9b176
Bruno <brunozoya@gmail.com>
2024-02-08 22:26:50 +0000

    initial commit

---
+++ views/about.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+def load_view():
+    st.title("Search")
+    st.write("Welcome to the about page")
---
+++ views/home.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+def load_view():
+    st.title("Home")
+    st.write("Welcome to the home page")
